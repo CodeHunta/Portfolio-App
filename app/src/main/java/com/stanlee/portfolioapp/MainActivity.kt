8ae8aa0a7a132347ddb685cf74e826b6a67b3556
Stanley Umezuruike <manstylo@gmail.com>
2020-05-06 11:52:34 +0100

    Add BottomSheet!

--- app/src/main/java/com/stanlee/portfolioapp/MainActivity.kt
+++ app/src/main/java/com/stanlee/portfolioapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.stanlee.portfolioapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_social.setOnClickListener {
+            val dialog = BottomSheetDialog(this)
+            val view = layoutInflater.inflate(R.layout.social_layout, null)
+            dialog.setContentView(view)
+            dialog.show()
+        }
     }
 }
