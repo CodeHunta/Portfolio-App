fe885500de9164cb9c722e4090577ae496caaaf2
Stanley Umezuruike <manstylo@gmail.com>
2020-05-08 22:36:14 +0100

    Declare functions!

--- app/src/main/java/com/stanlee/portfolioapp/MainActivity.kt
+++ app/src/main/java/com/stanlee/portfolioapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.stanlee.portfolioapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -20,16 +22,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun openEmail(view: View) {}
-    fun openGithub(view: View) {}
-    fun openTwitter(view: View) {}
-    fun openWhatsapp(view: View) {}
-    fun openFacebook(view: View) {}
-    fun openInstagram(view: View) {}
-    fun openLinkedIn(view: View) {}
-    fun openSkype(view: View) {}
-    fun openPlaystore(view: View) {}
-    fun openAndroid(view: View) {}
-    fun openChrome(view: View) {}
-    fun openYoutube(view: View) {}
+    fun openEmail(view: View) {
+        val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "stanleyumez@outlook.com", null))
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hello Stanley Umezuruike")
+        startActivity(emailIntent)
+    }
+    fun openGithub(view: View) {
+        val githubIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/CodeHunta"))
+        startActivity(githubIntent)
+    }
+    fun openTwitter(view: View) {
+        val twitterIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/CodeHunta?s=09"))
+        startActivity(twitterIntent)
+    }
+    fun openWhatsapp(view: View) {
+        val whatsappIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=+2348032324739"))
+        startActivity(whatsappIntent)
+    }
+    fun openFacebook(view: View) {
+        val fbIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://fb.com/manstylo"))
+        startActivity(fbIntent)
+    }
+    fun openInstagram(view: View) {
+        val igIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com/manstylo.ng"))
+        startActivity(igIntent)
+    }
+    fun openLinkedIn(view: View) {
+        val linkedinIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/stanley-umezuruike-1417a884"))
+        startActivity(linkedinIntent)
+    }
+    fun openSkype(view: View) {
+        val skypeIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://web.skype.com/stanleyumez?call"))
+        startActivity(skypeIntent)
+    }
+    fun openPlaystore(view: View) {
+        val playIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com"))
+        startActivity(playIntent)
+    }
+    fun openAndroid(view: View) {
+        val androidIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.android.com/kotlin/"))
+        startActivity(androidIntent)
+    }
+    fun openChrome(view: View) {
+        val chromeIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://bit.ly/OutWord"))
+        startActivity(chromeIntent)
+    }
+    fun openYoutube(view: View) {
+        val ytIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCr37EHkT42aJNX9hAzYArdw"))
+        startActivity(ytIntent)
+    }
 }
